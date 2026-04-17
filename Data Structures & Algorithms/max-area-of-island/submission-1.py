87ea69a129fbe0c20a232a015ad45fa222d9c860
Nitish C <45713563+n1tishc@users.noreply.github.com>
2026-04-17 14:52:49 -0400

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.py
@@ -0,0 +1,59 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        # We use DFS to traverse the graph
+
+        # rows, cols = len(grid), len(grid[0])
+        # visited = set()
+        # # directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+        # def dfs(r, c):
+
+        #     if (
+        #         r < 0 or c < 0 or r == rows or c == cols or 
+        #         grid[r][c] == 0 or (r,c) in visited
+        #     ):
+        #         return 0
+            
+        #     # All base cases pass
+        #     visited.add((r,c))
+
+        #     return (1 + dfs(r+1, c) +dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1))
+        
+        # area = 0
+
+        # for r in range(rows):
+        #     for c in range(cols):
+        #         area = max(dfs(r, c), area)
+        
+
+        # return area
+
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+
+        def dfs(r, c):
+
+            if (
+                r < 0 or c < 0 or r == rows or c == cols or
+                grid[r][c] == 0 or (r, c) in visited
+            ):
+                return 0
+            
+            # All base cases are skilled
+            visited.add((r, c))
+
+            return (
+                1 + dfs(r + 1, c)
+                + dfs(r - 1, c)
+                + dfs(r, c + 1)
+                + dfs(r, c - 1)
+            )
+        
+        area = 0
+
+        for row in range(rows):
+            for col in range(cols):
+                area = max(dfs(row, col), area)
+        
+        print(area)
+        return area
