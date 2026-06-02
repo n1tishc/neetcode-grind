4ea1109f81c2043b33a81f1fccc27967aa363d76
Nitish C <45713563+n1tishc@users.noreply.github.com>
2026-06-02 12:32:46 -0400

    Add: network-delay-time - submission-7

---
+++ Data Structures & Algorithms/network-delay-time/submission-7.py
@@ -0,0 +1,25 @@
+import heapq
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # Generate adjacencyy list
+        graph = defaultdict(list)
+
+        for u, v, time in times:
+            graph[u].append((v, time))
+        
+        # print(graph)
+        min_times = {}
+        min_heap = [(0, k)] # k -> source node
+
+        while min_heap:
+            k_to_i, i = heapq.heappop(min_heap)
+            if i in min_times:
+                continue
+            
+            min_times[i] = k_to_i
+
+            for nei, n_time in graph[i]:
+                if nei not in min_times:
+                    heapq.heappush(min_heap, (n_time + k_to_i, nei))
+        
+        return max(min_times.values()) if len(min_times) == n else -1
